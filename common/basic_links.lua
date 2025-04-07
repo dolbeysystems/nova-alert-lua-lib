7f777fa59f8ff65ad7fef043300b0d513243c999
Jeremy Pritchard <jpritchard@dolbey.com>
2025-04-07 18:27:45 -0400

    SME-1974

--- common/basic_links.lua
+++ common/basic_links.lua
@@ -606,6 +606,27 @@ return function(Account)
     end
 
     --------------------------------------------------------------------------------
+    --- Remove duplicate links from a header
+    ---
+    ---@param links CdiAlertLink[] The links to check for duplicates
+    --- 
+    --- @return CdiAlertLink[] - The unique links by discrete _id
+    --------------------------------------------------------------------------------
+    function module.remove_duplicate_links_in_header(links)
+        local discrete_id = {}
+        --- @type CdiAlertLink[]
+        local unique_links = {}
+
+        for _, link in ipairs(links) do
+            if link.discrete_value_id ~= nil and not discrete_id[link.discrete_value_id] then
+                discrete_id[link.discrete_value_id] = true
+                table.insert(unique_links, link)
+            end
+        end
+        return unique_links
+    end
+
+    --------------------------------------------------------------------------------
     --- Merge links with old links
     ---
     --- @param old_links CdiAlertLink[] The existing alert
