ec5f4a1edca342e7427327fffd9dfa2e7cda3650
JerPritchard <jpritchard@dolbey.com>
2025-06-26 18:15:29 -0400

    SME-2305

--- common/glasgow.lua
+++ common/glasgow.lua
@@ -2,47 +2,63 @@
 return function(Account)
     local module = {}
     local discrete = require("libs.common.discrete_values")(Account)
+    local codes = require("libs.common.codes")(Account)
     local dates = require "libs.common.dates"
+    local site_discretes = require "libs.common.site_discretes"
     local log = require("cdi.log")
     local cdi_alert_link = require "cdi.link"
+    ---------------------------------------------------------------------------------------------
+    --- Abstract link args class
+    ---------------------------------------------------------------------------------------------
+    --- @class (exact) GetGlasgowLinksArgs : LinkArgs
+    --- @field consecutive boolean? Determines if we are looking for consecutive values
+    --- @field glasgow_calculation number? The custom calculation for the Glasgow Coma Score.
+    ---------------------------------------------------------------------------------------------
 
 
-    --- @param glasgow_calculation number
-    --- @param consecutive boolean
-    --- @param dv_names_glasgow_coma_scale string[]
-    --- @param dv_names_glasgow_eye_opening string[]
-    --- @param dv_names_glasgow_verbal string[]
-    --- @param dv_names_glasgow_motor string[]
-    --- @param dv_names_oxygen_therapy string[]
+    --- @param args GetGlasgowLinksArgs
     --- @return CdiAlertLink[]
-    function module.glasgow_linked_values(
-        glasgow_calculation,
-        consecutive,
-        dv_names_glasgow_coma_scale,
-        dv_names_glasgow_eye_opening,
-        dv_names_glasgow_verbal,
-        dv_names_glasgow_motor,
-        dv_names_oxygen_therapy
-    )
+    function module.glasgow_linked_values(args)
+        local consecutive = args[1] or false
+        local glasgow_calculation = args[2] or 0
+
+        local code_link_g30 = codes.make_code_prefix_link("G30.", "Alzheimers Disease")
+        local abs_link_ch_baseline_mental_status = codes.make_abstraction_link("CHANGE_IN_BASELINE_MENTAL_STATUS", "Change in Baseline Mental Status")
+        local code_link_dementia_2 = codes.make_code_prefix_link("F02.", "Dementia")
+        local code_link_dementia_1 = codes.make_code_prefix_link("F01.", "Dementia")
+        local code_link_dementia_3 = codes.make_code_prefix_link("F03.", "Dementia")
+        if (
+                (code_link_dementia_1 or
+                    code_link_dementia_2 or
+                    code_link_dementia_3 or
+                    code_link_g30) and
+                abs_link_ch_baseline_mental_status == nil
+            ) and glasgow_calculation == 0
+        then
+            glasgow_calculation = site_discretes.calc_very_low_glasgow_coma_scale
+        elseif glasgow_calculation == 0 then
+            glasgow_calculation = site_discretes.calc_low_glasgow_coma_scale
+        end
+
         local dvs_score = discrete.get_ordered_discrete_values {
-            discreteValueNames = dv_names_glasgow_coma_scale,
+            discreteValueNames = site_discretes.dv_names_glasgow_coma_scale,
             -- the annotations on predicate suggest that this is always true
             predicate = function(dv_, num) return num ~= nil end
         }
         local dvs_eye = discrete.get_ordered_discrete_values {
-            discreteValueNames = dv_names_glasgow_eye_opening,
+            discreteValueNames = site_discretes.dv_names_glasgow_eye_opening,
             predicate = function(dv, num_) return dv.result ~= nil end
         }
         local dvs_verbal = discrete.get_ordered_discrete_values {
-            discreteValueNames = dv_names_glasgow_verbal,
+            discreteValueNames = site_discretes.dv_names_glasgow_verbal,
             predicate = function(dv, num_) return dv.result ~= nil end
         }
         local dvs_motor = discrete.get_ordered_discrete_values {
-            discreteValueNames = dv_names_glasgow_motor,
+            discreteValueNames = site_discretes.dv_names_glasgow_motor,
             predicate = function(dv, num_) return dv.result ~= nil end
         }
         local dvs_oxygen = discrete.get_ordered_discrete_values {
-            discreteValueNames = dv_names_oxygen_therapy,
+            discreteValueNames = site_discretes.dv_names_oxygen_therapy,
             predicate = function(dv, num_)
                 return dv.result ~= nil and
                     (string.find(dv.result, "vent") ~= nil or
