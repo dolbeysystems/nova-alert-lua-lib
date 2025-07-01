68a0ae2920d3e5405d17b72a36436f718e814f3e
JerPritchard <jpritchard@dolbey.com>
2025-07-01 12:26:34 -0400

    SME-2335

--- common/site_discretes.lua
+++ common/site_discretes.lua
@@ -3,67 +3,97 @@ local discrete_names = {}
 
 -- local site_discretes = require("libs.common.site_discretes")
 
--- Absolute Neutrophils - Immunocompromised
-discrete_names.dv_names_absolute_neutrophils = { "ABSOLUTE NEUTROPHILS (10x3/uL)" }
+-- Absolute Basophil - Pancytopenia
+discrete_names.dv_names_absolute_basophils = { "" }
+-- Absolute Eosinophil - Pancytopenia
+discrete_names.dv_names_absolute_eosinophils = { "EOSIN ABSOLUTE (10X3/uL)" }
+-- Absolute Lymphocytes - Pancytopenia
+discrete_names.dv_names_absolute_lymphocytes = { "" }
+-- Absolute Monocytes - Pancytopenia
+discrete_names.dv_names_absolute_monocytes = { "" }
+-- Absolute Neutrophils - Immunocompromised, Pancytopenia
+discrete_names.dv_names_absolute_neutrophils = { "ABSOLUTE NEUTROPHILS (10x3/uL)", "ABS NEUT COUNT (10x3/uL)" }
 -- Activated Clotting Time - Coagulopathy
 discrete_names.dv_names_activated_clotting_time = { "" }
 -- Acetone - Diabetes
 discrete_names.dv_names_acetone = { "" }
+-- Aldolase - Rhabdomyolysis
+discrete_names.dv_names_aldolase = { "ALDOLASE" }
 -- Alkaline Phos - Encephalopathy
 discrete_names.dv_names_alkaline_phos = { "ALKALINE PHOS", "ALK PHOS TOTAL (U/L)" }
--- ALT - Hypertensive Crisis
-discrete_names.dv_names_alt = { "ALT", "ALT/SGPT (U/L)	16-61" }
+-- ALT - Hypertensive Crisis, Sepsis-SIRS, Shock
+discrete_names.dv_names_alt = { "ALT", "ALT/SGPT (U/L)	16-61", "ALT (unit/L)" }
 -- Amphetamine Screen - Encephalopathy, Hypertensive Crisis
 discrete_names.dv_names_amphetamine_screen = { "AMP/METH UR", "AMPHETAMINE URINE" }
 -- Anion Gap - Acidosis
 discrete_names.dv_names_anion_gap = { "" }
--- AST - Hypertensive Crisis
-discrete_names.dv_names_ast = { "AST", "AST/SGOT (U/L)" }
+-- AST - Hypertensive Crisis, Sepsis-SIRS, Shock
+discrete_names.dv_names_ast = { "AST", "AST/SGOT (U/L)", "AST (unit/L)" }
 -- Base Excess - Acidosis
 discrete_names.dv_names_base_excess = { "BASE EXCESS (mmol/L)" }
+-- Basophil Auto - Pancytopenia
+discrete_names.dv_names_basophil_auto = { "BASOPHILS (%)", "BASOS (%)" }
 -- Barbiturate Screen - Encephalopathy
 discrete_names.dv_names_barbiturate_screen = { "BARBITURATES URINE", "BARBS UR" }
 -- Benzodiazepine Screen - Encephalopathy
 discrete_names.dv_names_benzodiazepine_screen = { "BENZO URINE", "BENZO UR" }
 -- Beta Hydroxybutyrate - Diabetes
 discrete_names.dv_names_beta_hydroxybutyrate = { "BETAHYDROXY BUTYRATE (mmol/L)" }
--- Bilirubin Total - Encephalopathy
-discrete_names.dv_names_bilirubin_total = { "TOTAL BILIRUBIN (mg/dL)" }
 -- Blood CO2 - Acidosis, Diabetes
 discrete_names.dv_names_blood_co2 = { "CO2 (mmol/L)" }
 -- Blood Glucose - Abnormal Serum Sodium, Acidosis, Encephalopathy, Sepis-Sirs, 
 discrete_names.dv_names_blood_glucose = { "GLUCOSE (mg/dL)", "GLUCOSE" }
 discrete_names.dv_names_blood_glucose_point_of_care = { "GLUCOSE ACCUCHECK (mg/dL)" }
+-- Blood in Urine - UTI
+discrete_names.dv_names_blood_in_urine = { "BLOOD" }
 -- Blood Loss - Anemia, Bleeding
 discrete_names.dv_names_blood_loss = { "" }
 -- BMI - Malnutrition, Morbid Obesity
 discrete_names.dv_names_bmi = { "3.5 BMI Calculation (kg/m2)" }
+-- BNP - Pulmonary Edema, Pulmonary Embolism
+discrete_names.dv_names_bnp = { "BNP(NT proBNP) (pg/mL)" }
 -- Braden Acitivity Score -Functional Quadriplegia
 discrete_names.dv_names_braden_activity_score = { "3.5 Activity (Braden Scale)" }
 -- Braden Mobility Score - Functional Quadriplegia
 discrete_names.dv_names_braden_mobility_score = { "3.5 Mobility" }
 -- Braden Score - Pressure Ulcer
 discrete_names.dv_names_braden_score = { "3.5 Braden Scale Total Points" }
+-- Breath Sounds - COPD
+discrete_names.dv_names_breath_sounds = { "" }
 -- Buprenorphine Screen - Encephalopathy
 discrete_names.dv_names_buprenorphine_screen = { "" }
--- C Blood - Encephalopathy
+-- C Blood - Encephalopathy, Pneumonia
 discrete_names.dv_names_c_blood = { "" }
--- C Urine - Encephalopathy
+-- C Reactive Protein - Pneumonia, Sepsis-SIRS
+discrete_names.dv_names_c_reactive_protein = { "C REACTIVE PROTEIN (mg/dL)" }
+-- C Urine - Encephalopathy, Sepsis-SIRS, UTI
 discrete_names.dv_names_c_urine = { "BACTERIA (/HPF)" }
 -- Cannabinoid Screen - Encephalopathy
 discrete_names.dv_names_cannabinoid_screen = { "CANNABINOIDS UR", "Cannabinoids (THC) UR" }
--- Central Venous Pressure - Heart Failure
+-- Cardiac Index - Shock
+discrete_names.dv_names_cardiac_index = { "Cardiac Index CAL cc", "FloTrac Cardiac Index CAL 0211" }
+-- Cardiac Ouput - Shock
+discrete_names.dv_names_cardiac_output = { "Cardiac Output cc", "FloTrac Cardiac Output CAL 0211 (1/min)" }
+-- Central Venous Pressure - Heart Failure, Shock
 discrete_names.dv_names_central_venous_pressure = { "CVP cc" }
 -- CIWA Score - Substance Abuse
 discrete_names.dv_names_ciwa_score = { "alcohol CIWA Calc score 1112" }
+-- CKMB - Rhabdomyolysis
+discrete_names.dv_names_ckmb = { "CKMB (ng/mL)" }
+-- CKMB Index - Rhabdomyolysis
+discrete_names.dv_names_ckmb_index = { "" }
 -- Cocaine Screen - Encephalopathy, Hypertensive Crisis
 discrete_names.dv_names_cocaine_screen = { "COCAINE URINE", "COCAINE UR CONF" }
+-- CPK - Rhabdomyolysis
+discrete_names.dv_names_cpk = { "CPK (U/L)" }
 -- Cryoprecipitate Transfusion - Bleeding
 discrete_names.dv_names_cryoprecipitate_transfusion = { "" }
 -- DBP - Encephalopathy, Hypertensive Crisis, Pulmonary Edema, Severe Sepsis, Shock, Stroke
 discrete_names.dv_names_dbp = { "BP Arterial Diastolic cc (mm Hg)", "DBP 3.5 (No Calculation) (mmhg)", "DBP 3.5 (No Calculation) (mm Hg)", "Diastolic Blood Pressure (mm Hg)" }
--- D-Dimer - Coagulopathy
+-- D-Dimer - Coagulopathy, Pulmonary Embolism
 discrete_names.dv_names_d_dimer = { "D-DIMER (mg/L FEU)" }
+-- Eosinophil Auto - Pancytopenia
+discrete_names.dv_names_eosinophil_auto = { "EOS (%)" }
 -- Ethanol Level - Encephalopathy
 discrete_names.dv_names_ethanol_level = { "ALCOHOL,ETHYL UR" }
 -- Fibrinogen - Coagulopathy
@@ -72,8 +102,6 @@ discrete_names.dv_names_fibrinogen = { "FIBRINOGEN (mg/dL)" }
 discrete_names.dv_names_fio2 = { "" }
 -- Glomerular Filtration Rate - Kidney Disease Chronic, Kidney Failure Acute
 discrete_names.dv_names_glomerular_filtration_rate = { "GFR (mL/min/1.73m2)" }
--- HCO3 - Acidosis
-discrete_names.dv_names_hco3 = { "HCO3 VENOUS (meq/L)" }
 -- Heart Rate - Acidosis, Acute MI, Atrial Fibrillation, Cerebral Edema, COPD, Diabetes, Heart Failure, Hypertensive Crisis, 
 --                  Pulmonary Edema, Pulmonary Embolism, Respiratory Failure, Rhabdomyolysis, Sepsis-SIRS, Severe Sepsis, Shock
 discrete_names.dv_names_heart_rate = { "Heart Rate (bpm)", "Heart Rate cc (bpm)", "3.5 Heart Rate (Apical) (bpm)", "3.5 Heart Rate (Other) (bpm)", "3.5 Heart Rate (Radial) (bpm)", "SCC Monitor Pulse (bpm)" }
@@ -91,14 +119,18 @@ discrete_names.dv_names_glasgow_eye_opening = { "3.5 Neuro Glasgow Eyes (Adult)"
 discrete_names.dv_names_glasgow_verbal = { "3.5 Neuro Glasgow Verbal (Adult)" }
 discrete_names.dv_names_glasgow_motor = { "3.5 Neuro Glasgow Motor" }
 discrete_names.dv_names_oxygen_therapy = { "DELIVERY", "Device Type" }
--- Influenza A Screen - Immunocompromised
+-- Influenza A Screen - COPD, Immunocompromised, Pneumonia
 discrete_names.dv_names_influenza_a = { "Influenza A" }
--- Influenza B Screen - Immunocompromised
+-- Influenza B Screen - COPD, Immunocompromised, Pneumonia
 discrete_names.dv_names_influenza_b = { "Influenza B" }
 -- INR - Bleeding, Coagulopathy, Sepsis-SIRS
 discrete_names.dv_names_inr = { "INR" }
+-- Interleukin 6 - Pneumonia, Sepsis-SIRS
+discrete_names.dv_names_interleukin_6 = { "INTERLEUKIN 6" }
 -- Intracranial Pressure - Cerebral Edema
 discrete_names.dv_names_intracranial_pressure = { "ICP cc (mm Hg)" }
+-- Lymphocyte Auto - Pancytopenia
+discrete_names.dv_names_lymphocyte_auto = { "LYMPHS (%)" }
 -- Lymphocyte Count - Malnutrition
 discrete_names.dv_names_lymphocyte_count = { "" }
 -- MAP - Acidosis, Acute MI, Anemia, Atrial Fibrillation, Hypertensive Crisis, Kidney Disease Chronic, 
@@ -112,6 +144,12 @@ discrete_names.dv_names_mcv = { "MCV (fL)" }
 discrete_names.dv_names_mchc = { "MCHC (g/dL)" }
 -- Methadone Screen - Encephalopathy
 discrete_names.dv_names_methadone_screen = { "METHADONE URINE", "METHADONE UR" }
+-- Monocyte Auto - Pancytopenia
+discrete_names.dv_names_monocyte_auto = { "MONOS (%)" }
+-- MRSA Screen - COPD, Pneumonia
+discrete_names.dv_names_mrsa_screen = { "MRSA DNA" }
+-- Neutrophil Auto - Pancytopenia
+discrete_names.dv_names_neutrophil_auto = { "" }
 -- Opiate Screen - Encephalopathy
 discrete_names.dv_names_opiate_screen = { "OPIATES URINE", "OPIATES UR" }
 -- Oxycodone Screen - Encephalopathy
@@ -121,11 +159,11 @@ discrete_names.dv_names_oxygen_flow_rate = { "Oxygen Flow Rate (L/min)", "Resp O
 -- Oxygen Therapy - Acute MI, COPD, Diabetes, Pneumonia, Pulmanary Edema, Pulmonary Embolism, Respiratory Failure, Shock
 discrete_names.dv_names_oxygen_therapy = { "DELIVERY", "Device Type", "FiO2" }
 -- PaO2 - Acidosis, Acute MI, COPD, Encephalopathy, Morbid Obesity, Pneumonia, Pulmonary Edema, Pulmonary Embolism, 
---            Respiratory Failure, Sepsis-SIRS, Shock
+--            Respiratory Failure, Sepsis-SIRS, Severe Sepsis Shock
 discrete_names.dv_names_pao2 = { "BLD GAS O2 (mmHg)", "PO2 (mmHg)" }
--- PAO2/FIO2 Site Calculated - COPD, Respiratory Failure, Sepsis-SIRS
+-- PAO2/FIO2 Site Calculated - COPD, Respiratory Failure, Sepsis-SIRS, Severe Sepsis
 discrete_names.dv_names_pao2_fio2 = { "PO2/FiO2 (mmHg)" }
--- PCO2 - Acidosis, Encephalopathy, Morbid Obesity, Respiratory Failure, Sepsis-SIRS
+-- PCO2 - Acidosis, Encephalopathy, Morbid Obesity, Pulmonary Edema, Respiratory Failure, Sepsis-SIRS
 discrete_names.dv_names_pco2 = { "pCO2 BldV (mm Hg)", "BLD GAS CO2 (mmHg)", "PaCO2 (mmHg)" }
 -- PH - Acidosis, Diabetes, Encephalopathy, Morbid Obesity, Pulmonary Edema, Respiratory Failure
 discrete_names.dv_names_ph = { "pH" }
@@ -133,37 +171,63 @@ discrete_names.dv_names_ph = { "pH" }
 discrete_names.dv_names_plasma_transfusion = { "Volume (mL)-Transfuse Plasma (mL)" }
 -- Platelet Count - Coagulopathy, Pancytopenia, Sepsis-SIRS, Severe Sepsis
 discrete_names.dv_names_platelet_count = { "PLATELET COUNT (10x3/uL)" }
--- Platelet Transfusion - Bleeding, Coagulopathy
+-- Platelet Transfusion - Bleeding, Coagulopathy, Pancytopenia
 discrete_names.dv_names_platelet_transfusion = { "" }
+-- Pleural Fluid Culture - COPD, Pneumonia
+discrete_names.dv_names_pleural_fluid_culture = { "" }
+-- Pneumococcal Antigen Screeen - COPD
+discrete_names.dv_names_pneumococcal_antigen = { "" }
+-- POC Lactate - Sepsis-SIRS, Severe Sepsis
+discrete_names.dv_names_poc_lactate = { "" }
 -- Pressure Injury Stage - Pressure Ulcer
 discrete_names.dv_names_pressure_injury_stage = { "" }
--- Pro BNP - Heart Failure
-discrete_names.dv_names_pro_bnp = { "BNP(NT proBNP) (pg/mL)" }
+-- Pro BNP - Heart Failure, Pulmonary Edema
+discrete_names.dv_names_pro_bnp = { "" }
+-- Procalcitonin - Pneumonia, Sepsis-SIRS
+discrete_names.dv_names_procalcitonin = { "PROCALCITONIN (ng/mL)" }
 -- Protein C Resistance - Coagulopathy
 discrete_names.dv_names_protein_c_resistance = { "" }
 -- PT - Bleeding, Coagulopathy
 discrete_names.dv_names_pt = { "PROTIME (SEC)" }
 -- PTT - Bleeding, Coagulopathy
 discrete_names.dv_names_ptt = { "PTT (SEC)" }
--- RBC - Anemia
+-- Pulmonary Pressure - Pulmonary Embolism
+discrete_names.dv_names_pulmonary_pressure = { "" }
+-- Pus in Urine - UTI
+discrete_names.dv_names_pus_in_urine = { "" }
+-- RBC - Anemia, Pancytopenia
 discrete_names.dv_names_rbc = { "RBC  (10X6/uL)" }
+-- Reduce Ejection Fraction - Pulmonary Edema
+discrete_names.dv_names_reduced_ejection_fraction = { "" }
+-- Respiratory Culture - Pneumonia
+discrete_names.dv_names_respiratory_culture = { "" }
+-- Respiratory Pattern - COPD
+discrete_names.dv_names_respiratory_pattern = { "" }
 -- RDW - Anemia
 discrete_names.dv_names_rdw = { "RDW CV (%)" }
--- Red Blood Cell Transfusion - Anemia
-discrete_names.dv_names_red_blood_cell_transfusion = { "" }
+-- Red Blood Cell Transfusion - Anemia, Pancytopenia, Shock
+discrete_names.dv_names_red_blood_cell_transfusion = { "Volume (mL)-Transfuse Red Blood Cells (mL)", "Volume (mL)-Transfuse Red Blood Cells, Irradiated (mL)" }
 -- Reticulocyte Count - Anemia
 discrete_names.dv_names_reticulocyte_count = { "" }
 -- Respiratory Rate - Acidosis, Cerebral Edema, COPD, Pneumonia, Pulmonary Edema, Pulmonary Embolism, Respiratory Failure, Sepsis-SIRS, Shock
 discrete_names.dv_names_respiratory_rate = { "3.5 Respiratory Rate (#VS I&O) (per Minute)", "Respiratory Rate (per Minute)" }
--- SARS Covid Screen - Immunocompromised
+-- RSV - Pneumonia
+discrete_names.dv_names_rsv = { "Respiratory syncytial virus" }
+-- SARS Covid Screen - COPD, Immunocompromised, Pneumonia
 discrete_names.dv_names_sars_covid = { "SARS-CoV2 (COVID-19)" }
+-- SARS Covid Antigen Screen - COPD
+discrete_names.dv_names_sars_covid_antigen = { "" }
 -- SBP - Acidosis, Acute MI, Anemia, Atrial Fibrillation, Encephalopathy, Hypertensive Crisis, Kidney Disease Chronic,
 --           Kidney Failure Acute, Pulmonary Edema, Rhabdomyolysis, Sepsis-SIRS, Severe Sepsis, Shock, Stroke, 
 discrete_names.dv_names_sbp = { "SBP 3.5 (No Calculation) (mm Hg)", "BP Arterial Systolic cc (mm Hg)", "Systolic Blood Pressure (mm Hg)" }
 -- Serum Ammonia - Encephalopathy
 discrete_names.dv_names_serum_ammonia = { "" }
+-- Serum Band - Sepsis-SIRS
+discrete_names.dv_names_serum_band = { "Band Neutrophils (%)" }
 -- Serum Bicarbonate - Acidosis, Diabetes, Respiratory Failure
-discrete_names.dv_names_serum_bicarbonate = { "HCO3 (meq/L)", "HCO3 (mmol/L)" }
+discrete_names.dv_names_serum_bicarbonate = { "HCO3 (meq/L)", "HCO3 (mmol/L)", "HCO3 BldA-sCnc (mmol/L)" }
+-- Serum Bilirubin - Encephalopathy, Sepsis-SIRS, Severe Sepsis
+discrete_names.dv_names_serum_bilirubin = { "TOTAL BILIRUBIN (mg/dL)" }
 -- Serum Blood Urea Nitrogen(BUN) - Acidosis, Encephalopathy, Hypertensive Crisis, Kidney Disease Chronic, Kidney Failure Acute, Rhabdomyolysis, Sepsis-SIRS, Shock,
 discrete_names.dv_names_serum_bun = { "BUN (mg/dL)" }
 -- Serum Calcium - Encephalopathy, Malnutrition
@@ -174,13 +238,13 @@ discrete_names.dv_names_serum_chloride = { "CHLORIDE (mmol/L)" }
 discrete_names.dv_names_serum_creatinine = { "CREATININE (mg/dL)", "CREATININE SERUM (mg/dL)" }
 -- Serum Ferritin - Anemia
 discrete_names.dv_names_serum_ferritin = { "FERRITIN (ng/mL)" }
--- Serum Folate - Anemia
+-- Serum Folate - Anemia, Pancytopenia
 discrete_names.dv_names_serum_folate = { "" }
 -- Serum Iron - Anemia
 discrete_names.dv_names_serum_iron = { "IRON TOTAL (ug/dL)" }
 -- Serum Ketone -- Acidosis
 discrete_names.dv_names_serum_ketone = { "" }
--- Serum Lactate - Acidosis
+-- Serum Lactate - Acidosis, Sepsis-SIRS, Shock
 discrete_names.dv_names_serum_lactate = { "LACTIC ACID (mmol/L)", "LACTATE (mmol/L)" }
 -- Serum Magnesium - Malnutrition
 discrete_names.dv_names_serum_magnesium = { "MAGNESIUM (mg/dL)" }
@@ -188,12 +252,14 @@ discrete_names.dv_names_serum_magnesium = { "MAGNESIUM (mg/dL)" }
 discrete_names.dv_names_serum_osmolality = { "OSMOLALITY (mOsm/kg)", "OSMO CALC (mOsm/kg)" }
 -- Serum Phosphate - Malnutrition
 discrete_names.dv_names_serum_phosphate = { "PHOSPHOROUS (mg/dL)", "PHOSPHATE (mg/dL)" }
--- Serum Potassium - Abnormal Serum Potassium, Acidosis, Malnutrition
+-- Serum Potassium - Abnormal Serum Potassium, Acidosis, Malnutrition, Rhabdomyolysis
 discrete_names.dv_names_serum_potassium = { "POTASSIUM (mmol/L)" }
 -- Serum Sodium - Abnormal Serum Sodium, Acidosis, Malnutrition
 discrete_names.dv_names_serum_sodium = { "SODIUM (mmol/L)" }
 -- SpO2 - Acidosis, Acute MI, COPD, Morbid Obesity, Pulmonary Edema, Respiratory Failure, Sepsis-SIRS, Severe Sepsis, 
 discrete_names.dv_names_spo2 = { "Pulse Oximetry(Num) (%)", "Pulse Oximetry (%)" }
+-- Sputum Culture - Pneumonia
+discrete_names.dv_names_sputum_culture = { "" }
 -- Temperature - Diabetes, Encephalopathy, Kidney Disease Chronic, Kidney Failure Acute, Pneumonia, Rhabdomyolysis, Sepsis-SIRS, Shock, UTI
 discrete_names.dv_names_temperature = { "Temperature Degrees C 3.5 (degrees C)", "Temperature  Degrees C 3.5 (degrees C)", "TEMPERATURE (C)" }
 -- Thrombin Time - Coagulopathy
@@ -204,21 +270,41 @@ discrete_names.dv_names_transferrin = { "TRANSFERRIN" }
 discrete_names.dv_names_total_cholesterol = { "CHOLESTEROL (mg/dL)", "CHOLESTEROL" }
 -- Total Iron Binding Capacity - Anemia
 discrete_names.dv_names_total_iron_binding_capacity = { "IRON BINDING" }
--- Troponin T High Sensitivity - Acute MI, Heart Failure, Hypertensive Crisis, Pulmonary Edema
+-- Troponin T High Sensitivity - Acute MI, Heart Failure, Hypertensive Crisis, Pulmonary Edema, Shock
 discrete_names.dv_names_troponin_t = { "TROPONIN, HIGH SENSITIVITY (ng/L)" }
--- UA Bacteria - Encephalopathy
-discrete_names.dv_names_ua_bacteria = { "BACTERIA (/HPF)" }
+-- UA Bacteria - Encephalopathy, UTI
+discrete_names.dv_names_ua_bacteria = { "" }
+-- UA Blood - UTI
+discrete_names.dv_names_ua_blood = { "BLOOD" }
+-- UA Hyaline Cast - UTI
+discrete_names.dv_names_ua_hyaline_cast = { "HYALINE CASTS (/LPF)" }
+-- UA Gran Cast - UTI
+discrete_names.dv_names_ua_gran_cast = { "" }
+-- UA Leak Esterase - UTI
+discrete_names.dv_names_ua_leak_est = { "LEUK ESTERASE" }
+-- UA Protein - UTI
+discrete_names.dv_names_ua_protein = { "PROTEIN (mg/dL)" }
+-- UA RBC - UTI
+discrete_names.dv_names_ua_rbc = { "RBC/HPF (/HPF)" }
+-- UA Squamous Epithelias - UTI
+discrete_names.dv_names_ua_squamous_epithelias = { "" }
+-- UA WBC - UTI
+discrete_names.dv_names_ua_wbc = { "WBC/HPF (/HPF)" }
+-- Urine Culture - Sepsis-SIRS
+discrete_names.dv_names_urine_culture = { "" }
 -- Urine Output - Kidney Disease Chronic
 discrete_names.dv_names_urine_output = { "" }
 -- Urine Ketones - Acidosis, Diabetes
 discrete_names.dv_names_urine_ketones = { "UR KETONES (mg/dL)", "KETONES (mg/dL)" }
 -- Urine Sodium - Kidney Disease Chronic, Kidney Failure Acute
 discrete_names.dv_names_urine_sodium = { "URINE SODIUM (mmol/L)" }
--- Venous Blood CO2 - Acidosis
+-- Venous Blood CO2 - Acidosis, Respiratory Failure
 discrete_names.dv_names_venous_blood_co2 = { "BLD GAS CO2 VEN (mmHg)" }
--- Venous PH - Acidosis, Morbid Obesity
+-- Venous HCO3 - Acidosis
+discrete_names.dv_names_venous_hco3 = { "HCO3 VENOUS (meq/L)" }
+-- Venous PH - Acidosis, Morbid Obesity, Respiratory Failure
 discrete_names.dv_names_venous_ph = { "pH (VENOUS)", "pH VENOUS" }
--- Vitamin B12 - Anemia
+-- Vitamin B12 - Anemia, Pancytopenia
 discrete_names.dv_names_vitamin_b12 = { "VITAMIN B12 (pg/mL)" }
 -- WBC - Anemia, Pancytopenia, Pneumonia, Sepsis-SIRS, UTI
 discrete_names.dv_names_wbc = { "WBC (10x3/ul)" }
